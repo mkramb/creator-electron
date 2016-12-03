fcc4c03672d44a31d650d0c226193374cce28a2d
Mitja Kramberger <mitja.kramberger@gmail.com>
2016-12-03 00:26:30 +0000

    Using console.log

--- commands/start.js
+++ commands/start.js
@@ -6,7 +6,7 @@ module.exports = {
       .option('-n, --name <name>', 'name of the project to run')
   },
   action: (config, options, callback) => {
-    this.log(`start ${options.name || config.name || 'now'}`)
+    console.log(`start ${options.name || config.name || 'now'}`)
     callback()
   }
 }
